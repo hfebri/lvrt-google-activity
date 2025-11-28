395fdb5c9df299189b14303b3e012e1d70141cc9
Helmi <hfebri@leverategroup.asia>
2025-11-28 16:27:05 +0700

    config

---
+++ .open-next/.build/open-next.config.mjs
@@ -0,0 +1,14 @@
+import { createRequire as topLevelCreateRequire } from 'module';const require = topLevelCreateRequire(import.meta.url);import bannerUrl from 'url';const __dirname = bannerUrl.fileURLToPath(new URL('.', import.meta.url));
+
+// open-next.config.ts
+var open_next_config_default = {
+  default: {
+    override: {
+      wrapper: "cloudflare",
+      converter: "edge"
+    }
+  }
+};
+export {
+  open_next_config_default as default
+};
